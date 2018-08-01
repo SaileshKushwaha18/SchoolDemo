ce8859ab0f4df2c6dcb9f0f95d714bd00494e61a
Sailesh Kushwaha <sailesh.kushwaha18@gmail.com>
2018-08-01 17:32:54 +0530

    Fixed PaymentHistories Issue
    Fixed PaymentHistories Issue

--- src/main/java/com/example/demo/model/StudentFee.java
+++ src/main/java/com/example/demo/model/StudentFee.java
@@ -13,9 +13,9 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
-import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
@@ -70,7 +70,7 @@ public class StudentFee implements Serializable{
     inverseJoinColumns={@JoinColumn(name="STUDENT_FEE_PARAMS_ID", referencedColumnName="STUDENT_FEE_PARAMS_ID")})
 	private List<StudentFeeParams> studentFeeParams;
 	
-	@OneToMany
+	@Transient
     @JsonIgnoreProperties("studentFee")
 	private List<StudentPaymentHistory> studentPaymentHistories;
 	
